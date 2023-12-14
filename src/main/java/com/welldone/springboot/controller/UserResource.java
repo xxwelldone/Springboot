657fcbe1e63d52b79dd6963417e160352baeb9f0
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2023-12-14 18:59:30 -0300

    Service layer and findbyId method

--- src/main/java/com/welldone/springboot/controller/UserResource.java
+++ src/main/java/com/welldone/springboot/controller/UserResource.java
@@ -1,19 +1,31 @@
 package com.welldone.springboot.controller;
 
 import com.welldone.springboot.model.User;
+import com.welldone.springboot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User x = new User(2L, "xxx", "xxx@gmail.com",
-                "88888", "123456");
-        return ResponseEntity.ok().body(x);
+    @Autowired
+    UserService userRepo;
 
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> response = userRepo.findAll();
+        return ResponseEntity.ok().body(response);
+
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User response = userRepo.findById(id);
+        return ResponseEntity.ok().body(response);
     }
 }
---
+++ src/main/java/com/welldone/springboot/service/UserService.java
@@ -0,0 +1,24 @@
+package com.welldone.springboot.service;
+
+import com.welldone.springboot.model.User;
+import com.welldone.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+
+    public User findById(Long id) {
+        return repo.findById(id).orElse(null);
+
+    }
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+
+}
