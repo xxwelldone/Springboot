3832af1cc6abc20273b78303af348ced60fe5049
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2023-12-21 11:27:04 -0300

    first commit in test

---
+++ src/main/java/com/welldone/springboot/model/OrderItems.java
@@ -0,0 +1,68 @@
+package com.welldone.springboot.model;
+
+import com.welldone.springboot.model.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_OrderItems")
+public class OrderItems implements Serializable {
+    @EmbeddedId
+    private OrderItemPK id;
+    private Integer qnt;
+    private Double price;
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+    public Product getProduct(Product product){
+        return id.getProduct();
+    }
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public OrderItems(){}
+
+    public OrderItems(Order order, Product product, Integer qnt, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.qnt = qnt;
+        this.price = price;
+    }
+
+    public Integer getQnt() {
+        return qnt;
+    }
+
+    public void setQnt(Integer qnt) {
+        this.qnt = qnt;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItems that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
